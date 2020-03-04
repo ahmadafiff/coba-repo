7a94e3623135f8ed084ea1bd4145513c44e332d5
ahmad afif <ahmadafifr@gmail.com>
2020-03-04 11:16:37 +0700

    edit jadi resolusi

--- ping.py
+++ ping.py
@@ -1,4 +1,8 @@
 import os;
-os.system("ping 8.8.8.8 -t 3");
-os.system("ping kaskus.com -t 5");
-os.system("ping facebook.com -t 6");
+os.system("sudo su");
+os.system("hujan123");
+os.system("cvt 1600 900");
+os.system("xrandr --newmode "1600x900_60.00"  118.25  1600 1696 1856 2112  900 903 908 934 -hsync +vsync");
+os.system("xrandr --addmode VGA-1 1600x900_60.00");
+
+
