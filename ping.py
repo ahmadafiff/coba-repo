140cee04de78f25a8526ce4bb0e50a0aee88a972
ahmad afif <ahmadafifr@gmail.com>
2020-03-04 13:51:23 +0700

    add ping google

--- ping.py
+++ ping.py
@@ -1,8 +1,3 @@
 import os;
-os.system("sudo su");
-os.system("hujan123");
-os.system("cvt 1600 900");
-os.system("xrandr --newmode "1600x900_60.00"  118.25  1600 1696 1856 2112  900 903 908 934 -hsync +vsync");
-os.system("xrandr --addmode VGA-1 1600x900_60.00");
-
+os.system("ping google.com");
 
